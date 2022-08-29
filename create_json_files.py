b2c5ded992dc43dc8bd41dac17c59d02909b7bbf
Tushar Joshi <52039538+tusharjoshi03@users.noreply.github.com>
2022-08-29 15:31:51 +0100

    Script to create JSON files for prices and currencies

--- create_json_files.py
+++ create_json_files.py
@@ -45,6 +45,56 @@ def create_products_json():
     with open('products_2.json', 'w') as prices_file:
         prices_file.write(product_prices_string)
         
+def create_currency_json():
+    
+    """creates a JSON file from a dictionary object containing currencies, associated symbols and their respective
+    conversion rates considering 'Euro' as the base currency
+
+        Parameters:
+        no parameters
+        
+        Returns:
+        no return value
+
+       """
+    
+    currency_conversion_rates = {
+        'currencies' : [
+            {
+                'currency' : 'EUR',
+                'rate' : 1.00,
+                'symbol': '€'
+            },
+            {
+                'currency' : 'USD',
+                'rate' : 1.00,
+                'symbol': '$'
+            },
+            {
+                'currency' : 'GBP',
+                'rate' : 0.85,
+                'symbol': '£'
+            },
+            {
+                'currency' : 'AUD',
+                'rate' : 1.45,
+                'symbol': '$'
+            },
+            {
+                'currency' : 'JPY',
+                'rate' : 137.05,
+                'symbol': '¥'
+            }
+        ]
+    }
+    
+    # convert dict into string format
+    currency_conversion_rates_string = json.dumps(currency_conversion_rates,indent=4)
+    
+    # write converted data to a JSON file
+    with open('currencies.json', 'w') as currencies_file:
+        currencies_file.write(currency_conversion_rates_string)
+        
 def main():
     
     """Main function that controls the flow of execution of this script. It calls a function that creates JSON file of
@@ -58,7 +108,11 @@ def main():
 
        """
     
+    # create JSON file for product prices
     create_products_json()
+    
+    # create JSON file for currency conversion rates
+    create_currency_json()
     
 if __name__=="__main__":
     
